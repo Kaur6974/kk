17321e721206828fced1c052b760316ad6d0337e
Kaur6974 <skaroramlt@gmail.com>
2019-05-15 11:02:43 -0400

    mid term practise

---
+++ src/student/Student.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author Ankush
+ */
+public class Student {
+    String name;
+    int age;
+    int sid;
+    
+    public Student(String a)
+    {
+    this.name = a;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+}
---
+++ src/student/TestStudent.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author Ankush
+ */
+public class TestStudent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // Student s1 = new Student();
+       // s1.setName("Arsh");
+        //System.out.println(s1.getName());
+        
+        Student s2 = new Student("Singla");
+        System.out.println(s2.getName());
+        
+        Student s3 = new Student("Bath");
+    }
+    
+}
